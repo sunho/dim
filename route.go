cbdd87104d3a1df944a49c4c2c103f7bd930bf47
sunho <ksunhokim123@naver.com>
2019-03-11 07:34:59 +0900

    Add utility funcs

--- route.go
+++ route.go
@@ -39,3 +39,87 @@ func (g *Group) Use(middlewares ...Middleware) {
 	}
 	g.Group.Use(middlewaresToFuncs(middlewares)...)
 }
+
+func (g *Group) Add(method, path string, handler echo.HandlerFunc, middlewares ...Middleware) *echo.Route {
+	for _, middleware := range middlewares {
+		g.d.inject(middleware)
+	}
+	return g.Group.Add(method, path, handler, middlewaresToFuncs(middlewares)...)
+}
+
+func (g *Group) Any(path string, handler echo.HandlerFunc, middlewares ...Middleware) []*echo.Route {
+	for _, middleware := range middlewares {
+		g.d.inject(middleware)
+	}
+	return g.Group.Any(path, handler, middlewaresToFuncs(middlewares)...)
+}
+
+func (g *Group) Match(methods []string, path string, handler echo.HandlerFunc, middlewares ...Middleware) []*echo.Route {
+	for _, middleware := range middlewares {
+		g.d.inject(middleware)
+	}
+	return g.Group.Match(methods, path, handler, middlewaresToFuncs(middlewares)...)
+}
+
+func (g *Group) GET(path string, handler echo.HandlerFunc, middlewares ...Middleware) *echo.Route {
+	for _, middleware := range middlewares {
+		g.d.inject(middleware)
+	}
+	return g.Group.GET(path, handler, middlewaresToFuncs(middlewares)...)
+}
+
+func (g *Group) POST(path string, handler echo.HandlerFunc, middlewares ...Middleware) *echo.Route {
+	for _, middleware := range middlewares {
+		g.d.inject(middleware)
+	}
+	return g.Group.POST(path, handler, middlewaresToFuncs(middlewares)...)
+}
+
+func (g *Group) PUT(path string, handler echo.HandlerFunc, middlewares ...Middleware) *echo.Route {
+	for _, middleware := range middlewares {
+		g.d.inject(middleware)
+	}
+	return g.Group.PUT(path, handler, middlewaresToFuncs(middlewares)...)
+}
+
+func (g *Group) PATCH(path string, handler echo.HandlerFunc, middlewares ...Middleware) *echo.Route {
+	for _, middleware := range middlewares {
+		g.d.inject(middleware)
+	}
+	return g.Group.PATCH(path, handler, middlewaresToFuncs(middlewares)...)
+}
+
+func (g *Group) CONNECT(path string, handler echo.HandlerFunc, middlewares ...Middleware) *echo.Route {
+	for _, middleware := range middlewares {
+		g.d.inject(middleware)
+	}
+	return g.Group.CONNECT(path, handler, middlewaresToFuncs(middlewares)...)
+}
+
+func (g *Group) DELETE(path string, handler echo.HandlerFunc, middlewares ...Middleware) *echo.Route {
+	for _, middleware := range middlewares {
+		g.d.inject(middleware)
+	}
+	return g.Group.DELETE(path, handler, middlewaresToFuncs(middlewares)...)
+}
+
+func (g *Group) TRACE(path string, handler echo.HandlerFunc, middlewares ...Middleware) *echo.Route {
+	for _, middleware := range middlewares {
+		g.d.inject(middleware)
+	}
+	return g.Group.TRACE(path, handler, middlewaresToFuncs(middlewares)...)
+}
+
+func (g *Group) OPTIONS(path string, handler echo.HandlerFunc, middlewares ...Middleware) *echo.Route {
+	for _, middleware := range middlewares {
+		g.d.inject(middleware)
+	}
+	return g.Group.OPTIONS(path, handler, middlewaresToFuncs(middlewares)...)
+}
+
+func (g *Group) HEAD(path string, handler echo.HandlerFunc, middlewares ...Middleware) *echo.Route {
+	for _, middleware := range middlewares {
+		g.d.inject(middleware)
+	}
+	return g.Group.HEAD(path, handler, middlewaresToFuncs(middlewares)...)
+}
