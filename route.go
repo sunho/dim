ae9868cbb42e5aa506445a895e2f1973602f99f8
Sunho Kim <ksunhokim123@naver.com>
2019-07-28 06:42:37 +0900

    Support custom context

--- route.go
+++ route.go
@@ -46,86 +46,86 @@ func (g *Group) Use(middlewares ...Middleware) {
 	g.Group.Use(middlewaresToFuncs(middlewares)...)
 }
 
-func (g *Group) Add(method, path string, handler echo.HandlerFunc, middlewares ...Middleware) *echo.Route {
+func (g *Group) Add(method, path string, handler interface{}, middlewares ...Middleware) *echo.Route {
 	for _, middleware := range middlewares {
 		g.d.inject(middleware)
 	}
-	return g.Group.Add(method, path, handler, middlewaresToFuncs(middlewares)...)
+	return g.Group.Add(method, path, convertHandler(handler), middlewaresToFuncs(middlewares)...)
 }
 
-func (g *Group) Any(path string, handler echo.HandlerFunc, middlewares ...Middleware) []*echo.Route {
+func (g *Group) Any(path string, handler interface{}, middlewares ...Middleware) []*echo.Route {
 	for _, middleware := range middlewares {
 		g.d.inject(middleware)
 	}
-	return g.Group.Any(path, handler, middlewaresToFuncs(middlewares)...)
+	return g.Group.Any(path, convertHandler(handler), middlewaresToFuncs(middlewares)...)
 }
 
-func (g *Group) Match(methods []string, path string, handler echo.HandlerFunc, middlewares ...Middleware) []*echo.Route {
+func (g *Group) Match(methods []string, path string, handler interface{}, middlewares ...Middleware) []*echo.Route {
 	for _, middleware := range middlewares {
 		g.d.inject(middleware)
 	}
-	return g.Group.Match(methods, path, handler, middlewaresToFuncs(middlewares)...)
+	return g.Group.Match(methods, path, convertHandler(handler), middlewaresToFuncs(middlewares)...)
 }
 
-func (g *Group) GET(path string, handler echo.HandlerFunc, middlewares ...Middleware) *echo.Route {
+func (g *Group) GET(path string, handler interface{}, middlewares ...Middleware) *echo.Route {
 	for _, middleware := range middlewares {
 		g.d.inject(middleware)
 	}
-	return g.Group.GET(path, handler, middlewaresToFuncs(middlewares)...)
+	return g.Group.GET(path, convertHandler(handler), middlewaresToFuncs(middlewares)...)
 }
 
-func (g *Group) POST(path string, handler echo.HandlerFunc, middlewares ...Middleware) *echo.Route {
+func (g *Group) POST(path string, handler interface{}, middlewares ...Middleware) *echo.Route {
 	for _, middleware := range middlewares {
 		g.d.inject(middleware)
 	}
-	return g.Group.POST(path, handler, middlewaresToFuncs(middlewares)...)
+	return g.Group.POST(path, convertHandler(handler), middlewaresToFuncs(middlewares)...)
 }
 
-func (g *Group) PUT(path string, handler echo.HandlerFunc, middlewares ...Middleware) *echo.Route {
+func (g *Group) PUT(path string, handler interface{}, middlewares ...Middleware) *echo.Route {
 	for _, middleware := range middlewares {
 		g.d.inject(middleware)
 	}
-	return g.Group.PUT(path, handler, middlewaresToFuncs(middlewares)...)
+	return g.Group.PUT(path, convertHandler(handler), middlewaresToFuncs(middlewares)...)
 }
 
-func (g *Group) PATCH(path string, handler echo.HandlerFunc, middlewares ...Middleware) *echo.Route {
+func (g *Group) PATCH(path string, handler interface{}, middlewares ...Middleware) *echo.Route {
 	for _, middleware := range middlewares {
 		g.d.inject(middleware)
 	}
-	return g.Group.PATCH(path, handler, middlewaresToFuncs(middlewares)...)
+	return g.Group.PATCH(path, convertHandler(handler), middlewaresToFuncs(middlewares)...)
 }
 
-func (g *Group) CONNECT(path string, handler echo.HandlerFunc, middlewares ...Middleware) *echo.Route {
+func (g *Group) CONNECT(path string, handler interface{}, middlewares ...Middleware) *echo.Route {
 	for _, middleware := range middlewares {
 		g.d.inject(middleware)
 	}
-	return g.Group.CONNECT(path, handler, middlewaresToFuncs(middlewares)...)
+	return g.Group.CONNECT(path, convertHandler(handler), middlewaresToFuncs(middlewares)...)
 }
 
-func (g *Group) DELETE(path string, handler echo.HandlerFunc, middlewares ...Middleware) *echo.Route {
+func (g *Group) DELETE(path string, handler interface{}, middlewares ...Middleware) *echo.Route {
 	for _, middleware := range middlewares {
 		g.d.inject(middleware)
 	}
-	return g.Group.DELETE(path, handler, middlewaresToFuncs(middlewares)...)
+	return g.Group.DELETE(path, convertHandler(handler), middlewaresToFuncs(middlewares)...)
 }
 
-func (g *Group) TRACE(path string, handler echo.HandlerFunc, middlewares ...Middleware) *echo.Route {
+func (g *Group) TRACE(path string, handler interface{}, middlewares ...Middleware) *echo.Route {
 	for _, middleware := range middlewares {
 		g.d.inject(middleware)
 	}
-	return g.Group.TRACE(path, handler, middlewaresToFuncs(middlewares)...)
+	return g.Group.TRACE(path, convertHandler(handler), middlewaresToFuncs(middlewares)...)
 }
 
-func (g *Group) OPTIONS(path string, handler echo.HandlerFunc, middlewares ...Middleware) *echo.Route {
+func (g *Group) OPTIONS(path string, handler interface{}, middlewares ...Middleware) *echo.Route {
 	for _, middleware := range middlewares {
 		g.d.inject(middleware)
 	}
-	return g.Group.OPTIONS(path, handler, middlewaresToFuncs(middlewares)...)
+	return g.Group.OPTIONS(path, convertHandler(handler), middlewaresToFuncs(middlewares)...)
 }
 
-func (g *Group) HEAD(path string, handler echo.HandlerFunc, middlewares ...Middleware) *echo.Route {
+func (g *Group) HEAD(path string, handler interface{}, middlewares ...Middleware) *echo.Route {
 	for _, middleware := range middlewares {
 		g.d.inject(middleware)
 	}
-	return g.Group.HEAD(path, handler, middlewaresToFuncs(middlewares)...)
+	return g.Group.HEAD(path, convertHandler(handler), middlewaresToFuncs(middlewares)...)
 }
--- utils.go
+++ utils.go
@@ -2,6 +2,8 @@ package dim
 
 import (
 	"reflect"
+
+	"github.com/labstack/echo"
 )
 
 func indirectType(t reflect.Type) reflect.Type {
@@ -120,4 +122,44 @@ func callFactory(factory, conf interface{}) (interface{}, error) {
 	}
 
 	return ptr(reflect.Indirect(vals[0])).Interface(), nil
+}
+
+func isHandler(handler interface{}) bool {
+	fn := reflect.ValueOf(handler)
+	if !fn.IsValid() {
+		return false
+	}
+	t := fn.Type()
+	if t.Kind() != reflect.Func {
+		return false
+	}
+	if t.NumIn() != 1 || t.NumOut() != 1 {
+		return false
+	}
+	if t.Out(0) != reflect.TypeOf((*error)(nil)).Elem() {
+		return false
+	}
+	return true
+}
+
+func callHandler(handler, context interface{}) error {
+	fn := reflect.ValueOf(handler)
+	t := fn.Type()
+
+	c := reflect.ValueOf(context)
+	t2 := c.Type()
+	if t.In(0) != t2 {
+		panic("Invalid context type: " + t2.Name() + " Expected: " + t.In(0).Name())
+	}
+	out := fn.Call([]reflect.Value{c})
+	return out[0].Interface().(error)
+}
+
+func convertHandler(handler interface{}) echo.HandlerFunc {
+	if !isHandler(handler) {
+		panic("Invalid handler")
+	}
+	return func(c echo.Context) error {
+		return callHandler(handler, c)
+	}
 }
